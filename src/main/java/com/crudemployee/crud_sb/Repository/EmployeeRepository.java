15777931e4af2a5354fa74e5091aa24bb99d57f0
Suyash Shrestha <sthasuyash11@gmail.com>
2024-05-06 13:49:37 +0545

    remove all

--- src/main/java/com/crudemployee/crud_sb/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crudemployee.crud_sb.Repository;
-
-import com.crudemployee.crud_sb.Employee.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
